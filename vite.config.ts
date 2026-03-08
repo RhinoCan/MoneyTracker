86eacf68c7268f929ea244f7f6fea23836d0ed62
RhinoCan <29071660+RhinoCan@users.noreply.github.com>
2026-03-08 10:26:01 -0400

    Commit after fixing errors in vite.config.ts re materialdesignicons.css

--- vite.config.ts
+++ vite.config.ts
@@ -6,19 +6,6 @@ import { fileURLToPath, URL } from "node:url";
 export default defineConfig({
   base: "/MoneyTracker/",
   plugins: [vue(), tsconfigPaths()],
-  server: {
-    fs: {
-      // We are expanding the allow list to include the parent directory
-      // where Vite is seeing the @mdi fonts.
-      allow: [
-        fileURLToPath(new URL("./", import.meta.url)), // Project root
-        "..", // Parent directory (C:/Vue/)
-      ],
-    },
-  },
-  optimizeDeps: {
-    include: ["@mdi/font/css/materialdesignicons.css"],
-  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
